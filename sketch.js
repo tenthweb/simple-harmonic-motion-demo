9efa42a8642bafad0e17d35c30a9a097a1e6dffd
tenthweb <47951574+tenthweb@users.noreply.github.com>
2026-02-14 20:08:34 +0000

    Modify sketch for 3D rendering and dynamics
    Updated canvas size and coordinates for 3D rendering. Adjusted velocity and background color.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-const fixedX = 200
-const fixedY = 100
-let freeX = 200
-let freeYOrigin =200
-let freeY = 200
+const fixedX = 0
+const fixedY = -150
+let freeX = 0
+let freeYOrigin =-0
+let freeY = -0
 const mass = 10
 const g = 9.8
 let t=0
@@ -10,13 +10,21 @@ let deltaT = .1
 let tension = 0
 let force =0
 let k = .1
-let velocity = 5
+let velocity = 20
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600, WEBGL);
+  noFill();
+  stroke(255);
 }
 
-function draw() {  
-  background(220);
+function draw() {
+  background(0);
+
+
+
+
   
   tension = k*(freeYOrigin-freeY)
   
@@ -29,12 +37,18 @@ function draw() {
   
   
   
+    let r = 100;
+  let pitch = (fixedY-freeY);
+  let L = 50;
+
+  beginShape();
+  for (let t = 0; t < PI*10; t += 0.05) {
+    vertex (r*sin(t), fixedY-t*pitch/20, r*cos(t));
+  }
+  endShape();
   
-  
-  line(fixedX,fixedY,freeX,freeY)
-  print(freeY)
-  print(force)
-  print(tension)
+ // line(fixedX,fixedY,freeX,freeY)
+
 
   t+=deltaT
 
